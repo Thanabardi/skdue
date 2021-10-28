544b6b4b76d80d4028bee7463ff0b0e60ad04c18
natchanon-space <natchanon.space@gmail.com>
2021-10-28 22:03:26 +0700

    #43 create test cases from api-v1 and change their path and add TODO in tests

---
+++ skdue_calendar/api/v2/tests/test_event_detail.py
@@ -0,0 +1,63 @@
+import json
+from datetime import datetime, timedelta
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+from skdue_calendar.models import Calendar, CalendarEvent, CalendarTag
+from .utils import convert_response
+
+
+class EventDetailTests(TestCase):
+    def setUp(self):
+        self.start_date = datetime.now().replace(microsecond=0)
+        self.end_date = self.start_date + timedelta(days=1)
+        user = User(username="tester")
+        user.save()
+        calendar = Calendar(name="calendar", slug="calendar", user=user)
+        calendar.save()
+        tag = CalendarTag(user=user, tag="event")
+        tag.save()
+        for i in range(3):
+            event = CalendarEvent(
+                calendar = calendar,
+                name = f"event {i}",
+                slug = f"event-{i}",
+                description = f"desc for event {i}",
+                start_date = self.start_date,
+                end_date = self.end_date,
+                tag = tag
+            )
+            event.save()
+
+    def test_get_object_not_found_from_not_exist_calendar(self):
+        """Raise 404 when calendar does not exist"""
+        calendar_slug = "not-exist-calendar"
+        event_slug = "event-0"
+        response = self.client.get(reverse('api_v2:event_detail', args=[calendar_slug, event_slug]))
+        self.assertEqual(404, response.status_code)
+
+    def test_get_object_not_fount_from_not_exist_event(self):
+        """Raise 404 when event does not exist"""
+        calendar_slug = "calendar"
+        event_slug = "not-exist-event"
+        response = self.client.get(reverse('api_v2:event_detail', args=[calendar_slug, event_slug]))
+        self.assertEqual(404, response.status_code)
+
+    def test_get(self):
+        """Returns detail of event"""
+        calendar_slug = "calendar"
+        event_slug = "event-0"
+        response = self.client.get(reverse('api_v2:event_detail', args=[calendar_slug, event_slug]))
+        response_data = convert_response(response.content)
+        expect = json.dumps({
+            "id": 1,
+            "calendar": 1,
+            "name": "event 0",
+            "slug": "event-0",
+            "get_absolute_url": "/calendar/event-0",
+            "description": "desc for event 0",
+            "start_date": self.start_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "end_date": self.end_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "tag_text": "event"
+        })
+        self.assertJSONEqual(expect, response_data)
