e518c05321be4aa1dcc458a2f3f7025306c8f78d
Kittison <kittison.j@ku.th>
2021-11-01 20:07:44 +0700

    add test generate tag

--- skdue_calendar/tests/test_utils.py
+++ skdue_calendar/tests/test_utils.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from skdue_calendar.utils import generate_slug
+from skdue_calendar.utils import generate_slug, generate_tag
 
 
 class GenerateSlugTests(TestCase):
@@ -19,3 +19,21 @@ class GenerateSlugTests(TestCase):
         for name, expect in zip(names, expects):
             with self.subTest():
                 self.assertEqual(expect, generate_slug(name))
+
+class GenerateTagTests(TestCase):
+    def test_generate_tag(self):
+        """Test that generate_tag returns correct tag"""
+        tags = ["event 1", "EvnTR 2", "evBMr 3 AND 5", 
+                "caLendar 1", "CaleENDAR TWO", "calendar 3 and 4"]
+        expects = ["event1", "evntr2", "evbmr3and5", 
+                "calendar1", "caleendartwo", "calendar3and4"]
+        for tag, expect in zip(tags, expects):
+            with self.subTest():
+                self.assertEqual(expect, generate_tag(tag))
+
+    def test_generate_tag_with_special_char(self):
+        tags = ["@123's test", "test's-file@%  "]
+        expects = ["123stest", "testsfile"]
+        for tag, expect in zip(tags, expects):
+            with self.subTest():
+                self.assertEqual(expect, generate_tag(tag))
