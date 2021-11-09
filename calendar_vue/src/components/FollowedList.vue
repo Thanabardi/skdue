9aa7540ad6e46f9c8512f19b6d3ab4b1b34c97ac
Kittison <kittison.j@ku.th>
2021-11-09 16:55:34 +0700

    #75 change to quick link to followed calendar

--- calendar_vue/src/components/FollowedList.vue
+++ calendar_vue/src/components/FollowedList.vue
@@ -1,28 +1,36 @@
 <template>
-    <div class="dropdown">
-        <input v-if="Object.keys(selectedItem).length === 0" ref="dropdowninput" v-model.trim="inputValue" class="dropdown-input"
-        type="text" placeholder="User follow list" />
-        <div v-else @click="resetSelection" class="dropdown-selected">
-            {{ selectedItem.followed_name }}
-        </div>
-        <div v-show="inputValue && apiLoaded" class="dropdown-list">
-            <div @click="selectItem(item)" v-show="itemVisible(item)" v-for="item in itemList" :key="item.followed_name" class="dropdown-item">
-                <div style="display:flex; justify-content:space-between">
-                    <div>{{ item.followed_name }}</div>
-                    <div>Calendars</div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <div class="user-detail-login">
+		<button class="app-button-tp" style="font-size: 22px;" 
+		@click="() => TogglePopup('buttonTrigger')">{{ this.calendar_slug }}</button>
+		<div v-if="popupTriggers.buttonTrigger" :TogglePopup="() => TogglePopup('buttonTrigger')">
+		    <div class="user-detail-tab" v-for="item in itemList" :key="item">
+				<button @click="redirectFollowedCalendar(item)" class="user-detail-button-tp"> {{ item.followed_name }}</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import axios from 'axios';
+import { ref } from 'vue';
 import Calendar from '../components/Calendar.vue'
 import EventDetails from '../components/EventDetails.vue'
 import { globalLocales } from '@fullcalendar/common'
 
 export default ({
+    setup () {
+		const popupTriggers = ref({
+			buttonTrigger: false,
+		});
+
+		const TogglePopup = (trigger) => {
+			popupTriggers.value[trigger] = !popupTriggers.value[trigger]
+		}
+		return {
+			popupTriggers,
+			TogglePopup
+		}
+	},
     data() {
 		return {
             selectedItem: {},
@@ -30,6 +38,7 @@ export default ({
             inputValue: '',
             itemList: [],
             apiLoaded: false,
+            calendar_slug: "",
 		}
 	},
     mounted () {
@@ -38,28 +47,16 @@ export default ({
     methods: {
         getFollowList() {
             // const calendar_slug = this.$route.params.calendar_slug
+            const calendar_slug = this.$route.params.calendar_slug
+            this.calendar_slug = calendar_slug.replace(/-/g,' ')
             axios.get(`/api/v2/me/follow`).then( response => {
                 console.log("fs data=", response.data)
                 this.itemList = response.data
-                this.apiLoaded = true
             })
         },
-        itemVisible (item) {
-            let currentName = item.followed_name.toLowerCase()
-            let currentInput = this.inputValue.toLowerCase()
-            return currentName.includes(currentInput)
-        },
-        resetSelection () {
-            this.selectedItem = {}
-            this.$nextTick( () => this.$refs.dropdowninput.focus() )
-            this.$emit('on-item-reset')
-        },
-        async selectItem (theItem) {
-            this.selectedItem = theItem 
-            this.inputValue = ''
-            this.$emit('on-item-selected', theItem)
-            await this.$router.push({ path: `/calendar/${theItem.followed_name}` })
-            this.$router.go()
+        async redirectFollowedCalendar(theItem) {
+			await this.$router.push({ path: `/me/` })
+			await this.$router.replace({ path: `/calendar/${theItem.followed_name}` })
         },
 	}
 })
@@ -67,70 +64,74 @@ export default ({
 
 
 <style scoped>
-.dropdown{
-  position: absolute;
-  width: 85%;
-  padding-top: 0px;
-  left: 35%;
-  top: 0px;
-  transform: translate(-50%);
-  /* max-width: 400px; */
-  /* margin: 0 auto; */
+.user-detail-app-button {
+    line-height: 0px;
+    font-size: 40px;
+    font-weight: 500px;
+    position: fixed;
+    left: 2%;
+    background: none;
+    border: none;
+    color: var(--white);
+    cursor: pointer;
+    text-decoration: none;
 }
-.dropdown-input {
-	background: var(--main-green);
-	font-size: 20px;
-	padding: 8px 20px;
-	width:100%;
-	border: none;
-	border-radius: 4px;
+.user-detail-app-button:active {
+	color: var(--white-dark);
 }
-/* .dropdown-input:focus, .dropdown-selected:hover{
-  background: #fff;
-  border-color: #e2e8f0;
+.user-detail {
+    position: absolute;
+    right: 2%;
+	top: 10px;
 }
-.dropdown-input::placeholder{
-  opacity: 0.7;
+.user-detail-not-login {
+	display: flex;
+	justify-content: space-evenly;
+	top: 6px;
+	width: 320px;
 }
-.dropdown-selected{
-  font-weight: bold;
-  cursor: pointer;
-} */
-.dropdown-list{
-  background: var(--white);
-  position: absolute;
-  width: 105%;
-  max-height: 500px;
-  margin-top: 4px;
-  overflow-x: hidden;
-  box-shadow: 0px 0px 1px 0px var(--black-op-1), 0px 0px 40px 0px var(--black-op-2);
-  border-radius: 2px;
-  font-size: 20px;
-  color: var(--black);
+.user-detail-login {
+	width: 250px;
+    border: 1px solid transparent;
+    padding: 5px;
+	transform: translate(0, -20px);
 }
-.dropdown-item{
-  padding: 5px 16px;
-  width: 96%;
-  cursor: pointer;
+.user-detail-login:hover {
+	border: 1px solid var(--white-op-1);
 }
-.dropdown-item:hover{
-  background: var(--white-dark);
+.user-detail-tab {
+    background-color: var(--white);
+    box-shadow: 0px 0px 1px 0px var(--black-op-1), 0px 0px 40px 0px var(--black-op-2);
+    position: absolute;
+    width: 262px;
+    margin-top: 10px;
+    overflow-x: hidden;
+    right: 0;
+    border-radius: 2px;
+    text-align: left;
 }
-.dropdown-item-flag{
-  max-width: 24px;
-  max-height: 18px;
-  margin: auto 12px auto 0px;
+.user-detail-button-tp {
+    background: none;
+    border: none;
+    color: var(--black);
+    cursor: pointer;
+    font-size: 18px;
+    width: 100%;
+    padding: 8px 15px 8px 15px;
+    text-align: left;
 }
-::placeholder {
-  color: white;
-  opacity: 1; /* Firefox */
+.user-detail-button-tp:hover {
+    background-color: var(--green);
+    color: var(--white);
 }
-
-:-ms-input-placeholder { /* Internet Explorer 10-11 */
- color: white;
-}
-
-::-ms-input-placeholder { /* Microsoft Edge */
- color: white;
+.user-detail-button {
+    color: var(--white);
+    cursor: pointer;
+    font-size: 22px;
+    width: 100px;
+    padding: 4px 15px 4px 15px;
+    text-align: center;
+	border-radius: 40px;
+	text-decoration: none;
 }
 </style>
