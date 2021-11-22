94af669b4fa6a86b6b08fc6a6410bff5abebec58
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-11-22 22:00:16 +0700

    #74 401 handle page

--- calendar_vue/src/views/Error401.vue
+++ calendar_vue/src/views/Error401.vue
@@ -1,9 +1,9 @@
 <template>
     <center class="center"><div class="error">
         <div class="box">
-            <h3 style="font-size: 150px;"> ERROR 401 </h3>
-            <p style="font-size: 100px; border-top: double;">Unauthorized</p>
-            <div><router-link style="font-size: 45px" to='/'>Go to login page</router-link></div>
+            <div class="box-code"><h2 style="font-size: 100px;"> ERROR 401 </h2></div>
+            <p style="font-size: 25px;">Unauthorized: The page that you looking for require login.</p>
+            <router-link to='/'><button class="button" style="font-size: 25px;">Login page</button></router-link>
         </div>
     </div></center>
 </template>
@@ -16,10 +16,33 @@ export default ({
 
 <style scoped>
 .center {
-  position: absolute;
-  top: 50%;
-  width: 100%;
-  text-align: center;
-  font-size: 18px;
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    text-align: center;
+}
+
+.box {
+    border: 2px solid #c0ca35;
+    background-color: #c0ca35;
+    padding: 10px;
+    border-radius: 15px 85px;
+    font-size: 100px;
+    width: 70%;
+    margin: auto;
+}
+
+.router-link {
+    text-decoration: none;
+    appearance: button;
+}
+
+.button {
+    border-radius: 20px;
+    border: none;
+    font-weight: bold;
+    padding: 10px 40px;
+    text-transform: uppercase;
+    cursor: pointer;
 }
 </style>
