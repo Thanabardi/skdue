4189c0738f9fb2b2c49511e473f695e28289f075
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-11-23 16:46:53 +0700

    #74 500 error page

--- calendar_vue/src/views/Error5xx.vue
+++ calendar_vue/src/views/Error5xx.vue
@@ -1,10 +1,10 @@
 <template>
     <center class="center"><div class="error">
         <div class="box">
-            <h3 style="font-size: 150px;"> ERROR 500 </h3>
-            <p style="font-size: 30px; border-top: double;">Things are a little unstable here</p>
-            <p style="font-size: 30px;"> I suggest come back later</p>
-            <div><router-link style="font-size: 45px" to='/'>Go to login page</router-link></div>
+            <h3 style="font-size: 100px;"> ERROR 500 </h3>
+            <p style="font-size: 25px;">Internal server error</p>
+            <p style="font-size: 25px;">Please come back later</p>
+            <!-- <div><router-link style="font-size: 45px" to='/'>Go to login page</router-link></div> -->
         </div>
     </div></center>
 </template>
@@ -17,10 +17,33 @@ export default ({
 
 <style scoped>
 .center {
-  position: absolute;
-  top: 50%;
-  width: 100%;
-  text-align: center;
-  font-size: 18px;
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    text-align: center;
+}
+
+.box {
+    border: 2px solid #c0ca35;
+    background-color: #c0ca35;
+    padding: 10px;
+    border-radius: 15px 85px;
+    font-size: 100px;
+    width: 70%;
+    margin: auto;
+}
+
+.router-link {
+    text-decoration: none;
+    appearance: button;
+}
+
+.button {
+    border-radius: 20px;
+    border: none;
+    font-weight: bold;
+    padding: 10px 40px;
+    text-transform: uppercase;
+    cursor: pointer;
 }
 </style>
